f1e8fac365ce507cc8b3ea71244532c19760fa47
bp2008 <bp2008@users.noreply.github.com>
2020-10-12 18:46:22 -0600

    Added Compass class and CompassDirection enum and tests.

---
+++ BPUtil/Compass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPUtil
+{
+	/// <summary>
+	/// Enum consisting of all the directions on a 16-point compass rose.
+	/// </summary>
+	public enum CompassDirection
+	{
+		N = 0,
+		NNE = 1,
+		NE = 2,
+		ENE = 3,
+		E = 4,
+		ESE = 5,
+		SE = 6,
+		SSE = 7,
+		S = 8,
+		SSW = 9,
+		SW = 10,
+		WSW = 11,
+		W = 12,
+		WNW = 13,
+		NW = 14,
+		NNW = 15
+	}
+	/// <summary>
+	/// Computes compass-related things.
+	/// </summary>
+	public static class Compass
+	{
+		/// <summary>
+		/// Returns the CompassDirection enum value that best describes the absolute bearing (degrees clockwise from True North).
+		/// </summary>
+		/// <param name="absoluteBearingDegrees">Degrees absolute bearing (0 = north, 90 = east, 180 = south, 270 = west)</param>
+		/// <returns></returns>
+		public static CompassDirection GetCompassDirection(int absoluteBearingDegrees)
+		{
+			absoluteBearingDegrees = absoluteBearingDegrees % 360;
+			if (absoluteBearingDegrees < 0)
+				absoluteBearingDegrees = 360 + absoluteBearingDegrees;
+			int idx = (int)Math.Round(absoluteBearingDegrees / 22.5);
+			if (idx < 0 || idx > 15)
+				idx = 0;
+			return (CompassDirection)idx;
+		}
+	}
+}
---
+++ UnitTests/TestCompass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BPUtil;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class TestCompass
+	{
+
+		[TestMethod]
+		public void TestGetCompassDirection_Wrapping()
+		{
+			Assert.AreEqual(CompassDirection.W, Compass.GetCompassDirection(-450));
+			Assert.AreEqual(CompassDirection.N, Compass.GetCompassDirection(-360));
+			Assert.AreEqual(CompassDirection.E, Compass.GetCompassDirection(-270));
+			Assert.AreEqual(CompassDirection.S, Compass.GetCompassDirection(-180));
+			Assert.AreEqual(CompassDirection.W, Compass.GetCompassDirection(-90));
+			Assert.AreEqual(CompassDirection.N, Compass.GetCompassDirection(0));
+			Assert.AreEqual(CompassDirection.E, Compass.GetCompassDirection(90));
+			Assert.AreEqual(CompassDirection.S, Compass.GetCompassDirection(180));
+			Assert.AreEqual(CompassDirection.W, Compass.GetCompassDirection(270));
+			Assert.AreEqual(CompassDirection.N, Compass.GetCompassDirection(360));
+			Assert.AreEqual(CompassDirection.E, Compass.GetCompassDirection(450));
+			Assert.AreEqual(CompassDirection.S, Compass.GetCompassDirection(540));
+			Assert.AreEqual(CompassDirection.W, Compass.GetCompassDirection(630));
+			Assert.AreEqual(CompassDirection.N, Compass.GetCompassDirection(720));
+			Assert.AreEqual(CompassDirection.E, Compass.GetCompassDirection(810));
+		}
+		[TestMethod]
+		public void TestGetCompassDirection_Precision()
+		{
+			for (int i = 0; i < 360; i++)
+			{
+				CompassDirection d = Compass.GetCompassDirection(i);
+				if (i < 12)
+					Assert.AreEqual(CompassDirection.N, d);
+				else if (i < 34)
+					Assert.AreEqual(CompassDirection.NNE, d);
+				else if (i < 57)
+					Assert.AreEqual(CompassDirection.NE, d);
+				else if (i < 79)
+					Assert.AreEqual(CompassDirection.ENE, d);
+				else if (i < 102)
+					Assert.AreEqual(CompassDirection.E, d);
+				else if (i < 124)
+					Assert.AreEqual(CompassDirection.ESE, d);
+				else if (i < 147)
+					Assert.AreEqual(CompassDirection.SE, d);
+				else if (i < 169)
+					Assert.AreEqual(CompassDirection.SSE, d);
+				else if (i < 192)
+					Assert.AreEqual(CompassDirection.S, d);
+				else if (i < 214)
+					Assert.AreEqual(CompassDirection.SSW, d);
+				else if (i < 237)
+					Assert.AreEqual(CompassDirection.SW, d);
+				else if (i < 259)
+					Assert.AreEqual(CompassDirection.WSW, d);
+				else if (i < 282)
+					Assert.AreEqual(CompassDirection.W, d);
+				else if (i < 304)
+					Assert.AreEqual(CompassDirection.WNW, d);
+				else if (i < 327)
+					Assert.AreEqual(CompassDirection.NW, d);
+				else if (i < 349)
+					Assert.AreEqual(CompassDirection.NNW, d);
+				else
+					Assert.AreEqual(CompassDirection.N, d);
+			}
+		}
+	}
+}
