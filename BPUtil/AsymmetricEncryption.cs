5f48e0f063cb0ce13c9c4d5bee594fdc267544b6
bp2008 <bp2008@users.noreply.github.com>
2022-07-05 10:47:13 -0600

    Added AsymmetricEncryption class and tests.

---
+++ BPUtil/AsymmetricEncryption.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPUtil
+{
+	/// <summary>
+	/// Enumerates cryptographic keystores available in Windows.
+	/// </summary>
+	public enum Keystore
+	{
+		/// <summary>
+		/// The key is kept at the machine level so that all users can access it (in theory)
+		/// </summary>
+		Machine,
+		/// <summary>
+		/// The key is kept at the user  level so only the user who created the key can access it (in theory).
+		/// </summary>
+		User
+	}
+	/// <summary>
+	/// <para>Performs asymmetric encryption and decryption using RSACryptoServiceProvider and OAEP padding. This class is thread-safe.</para>
+	/// <para>This class can also be used to generate and export keys for later use.</para>
+	/// </summary>
+	public static class AsymmetricEncryption
+	{
+		/// <summary>
+		/// RSA key size.  Defaults to 4096.  Key size mainly affects speed of key generation, and 4096 takes a couple seconds but is more secure than smaller key sizes. 
+		/// </summary>
+		public static int keySize = 4096;
+		/// <summary>
+		/// Generates new 4096-bit RSA keys without persisting them in the operating system's keystore.
+		/// </summary>
+		/// <param name="publicKeyBase64">CspBlob containing public key information, base64 encoded. Can be used for encrypting using <see cref="EncryptWithKey"/>.</param>
+		/// <param name="privateKeyBase64">The private (and public) components of the RSA key, base64 encoded. Can be used for encrypting using <see cref="EncryptWithKey"/> and decrypting using <see cref="DecryptWithKey"/>.</param>
+		public static void GenerateNewKeys(out string publicKeyBase64, out string privateKeyBase64)
+		{
+			using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(keySize))
+			{
+				publicKeyBase64 = Convert.ToBase64String(rsa.ExportCspBlob(false));
+				privateKeyBase64 = Convert.ToBase64String(rsa.ExportCspBlob(true));
+			}
+		}
+		/// <summary>
+		/// <para>Generates new 4096-bit RSA keys, saving them in a key container in the operating system's keystore.</para>
+		/// <para>The public key is exported via an out string parameter, but the private key information is not exported.</para>
+		/// <para>If you need to export the private key, you shouldn't bother using the operating system's keystore.</para>
+		/// </summary>
+		/// <param name="keystore">Specify which keystore the key should be saved in.</param>
+		/// <param name="keyContainerName">A string which uniquely identifies this encryption key among all other keys in the keystore.  You'll use it again to access the encryption key later.  E.g. "MyKeyForThisSpecificPurpose".</param>
+		/// <param name="publicKeyBase64">CspBlob containing public key information, base64 encoded. Can be used for encrypting using <see cref="EncryptWithKey"/>.</param>
+		public static void GenerateNewKeysInKeystore(Keystore keystore, string keyContainerName, out string publicKeyBase64)
+		{
+			CspParameters cspParams = CreateCspParameters(keystore, keyContainerName);
+			using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(keySize, cspParams))
+			{
+				rsa.PersistKeyInCsp = true;
+				publicKeyBase64 = Convert.ToBase64String(rsa.ExportCspBlob(false));
+			}
+		}
+		/// <summary>
+		/// Returns the public key from the RSA key in the specified key container. Returns null if the named key container does not exist in the specified keystore.
+		/// </summary>
+		/// <param name="keystore">Specify which keystore the key should be loaded from.</param>
+		/// <param name="keyContainerName">A string which uniquely identifies this encryption key among all other keys in the keystore.</param>
+		/// <returns></returns>
+		public static string GetPublicKeyFromKeystore(Keystore keystore, string keyContainerName)
+		{
+			CspParameters cspParams = CreateCspParameters(keystore, keyContainerName);
+			cspParams.Flags |= CspProviderFlags.UseExistingKey;
+			RSACryptoServiceProvider rsa;
+			try
+			{
+				rsa = new RSACryptoServiceProvider(cspParams);
+			}
+			catch (CryptographicException)
+			{
+				return null; // Thrown if the key does not exist, because of CspProviderFlags.UseExistingKey
+			}
+			try
+			{
+				return Convert.ToBase64String(rsa.ExportCspBlob(false));
+			}
+			finally
+			{
+				rsa.Dispose();
+			}
+		}
+		/// <summary>
+		/// Deletes the RSA key with the specified key container name.
+		/// </summary>
+		/// <param name="keystore">Specify which keystore the key should be deleted from.</param>
+		/// <param name="keyContainerName">A string which uniquely identifies this encryption key among all other keys in the keystore.</param>
+		public static void DeletePublicKeyFromKeystore(Keystore keystore, string keyContainerName)
+		{
+			CspParameters cspParams = CreateCspParameters(keystore, keyContainerName);
+			cspParams.Flags |= CspProviderFlags.UseExistingKey;
+			RSACryptoServiceProvider rsa;
+			try
+			{
+				rsa = new RSACryptoServiceProvider(keySize, cspParams);
+			}
+			catch (CryptographicException)
+			{
+				return; // Thrown if the key does not exist, because of CspProviderFlags.UseExistingKey
+			}
+			try
+			{
+				rsa.PersistKeyInCsp = false;
+				rsa.Clear();
+			}
+			finally
+			{
+				rsa.Dispose();
+			}
+		}
+		/// <summary>
+		/// Encrypts the given data using the given public or private key that is base64 encoded.
+		/// </summary>
+		/// <param name="publicKeyBase64">CspBlob containing public key information, base64 encoded, as exported using the <see cref="GenerateNewKeys"/> method. Can also accept the private key because RSA private keys typically include public key information too.</param>
+		/// <param name="data">Data to encrypt.</param>
+		/// <returns></returns>
+		public static byte[] EncryptWithKey(string publicKeyBase64, byte[] data)
+		{
+			using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+			{
+				rsa.ImportCspBlob(Convert.FromBase64String(publicKeyBase64));
+				return rsa.Encrypt(data, true);
+			}
+		}
+		/// <summary>
+		/// Decrypts the given data using the given public or private key that is base64 encoded.
+		/// </summary>
+		/// <param name="privateKeyBase64">CspBlob containing private key information, as a base64 string, as exported using the <see cref="GenerateNewKeys"/> method.</param>
+		/// <param name="data">Data to decrypt.</param>
+		/// <returns></returns>
+		public static byte[] DecryptWithKey(string privateKeyBase64, byte[] data)
+		{
+			using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+			{
+				rsa.ImportCspBlob(Convert.FromBase64String(privateKeyBase64));
+				if (rsa.PublicOnly)
+					throw new ApplicationException("The given key string did not contain private key components, and therefore cannot be used for decrypting.");
+				return rsa.Decrypt(data, true);
+			}
+		}
+		/// <summary>
+		/// Encrypts the given data using a key from the operating system's keystore.
+		/// </summary>
+		/// <param name="keystore">Specify which keystore the key should be loaded from.</param>
+		/// <param name="keyContainerName">A string which uniquely identifies this encryption key among all other keys in the keystore.</param>
+		/// <param name="data">Data to encrypt.</param>
+		/// <returns></returns>
+		public static byte[] EncryptWithKeyFromKeystore(Keystore keystore, string keyContainerName, byte[] data)
+		{
+			CspParameters cspParams = CreateCspParameters(keystore, keyContainerName);
+			using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cspParams))
+			{
+				return rsa.Encrypt(data, true);
+			}
+		}
+		/// <summary>
+		/// Decrypts the given data using a key from the operating system's keystore.
+		/// </summary>
+		/// <param name="keystore">Specify which keystore the key should be loaded from.</param>
+		/// <param name="keyContainerName">A string which uniquely identifies this encryption key among all other keys in the keystore.</param>
+		/// <param name="data">Data to decrypt.</param>
+		/// <returns></returns>
+		public static byte[] DecryptWithKeyFromKeystore(Keystore keystore, string keyContainerName, byte[] data)
+		{
+			CspParameters cspParams = CreateCspParameters(keystore, keyContainerName);
+			using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cspParams))
+			{
+				if (rsa.PublicOnly)
+					throw new ApplicationException("The given key string did not contain private key components, and therefore cannot be used for decrypting.");
+				return rsa.Decrypt(data, true);
+			}
+		}
+		/// <summary>
+		/// Returns a new CspParameters object configured for the specified keystore and key container name.
+		/// </summary>
+		/// <param name="keystore">Specify which keystore should be used.</param>
+		/// <param name="keyContainerName">A string which uniquely identifies this encryption key among all other keys in the keystore.</param>
+		/// <returns></returns>
+		private static CspParameters CreateCspParameters(Keystore keystore, string keyContainerName)
+		{
+			CspParameters cspParams = new CspParameters(1); // 1 for RSA.  This is also the default if omitted.
+			cspParams.KeyContainerName = keyContainerName;
+			if (keystore == Keystore.Machine)
+				cspParams.Flags = CspProviderFlags.UseMachineKeyStore;
+			return cspParams;
+		}
+	}
+}
--- UnitTests/TestEncryption.cs
+++ UnitTests/TestEncryption.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,12 @@ namespace UnitTests
 	[TestClass]
 	public class TestEncryption
 	{
+		[TestInitialize()]
+		public void Startup()
+		{
+			AsymmetricEncryption.keySize = 1024; // Small key size makes the tests run a lot faster.
+		}
+
 		[TestMethod]
 		public void TestSymmetricEncryption()
 		{
@@ -31,6 +38,150 @@ namespace UnitTests
 
 			Assert.AreEqual(string.Join(",", cipher2), string.Join(",", cipher), "encrypted buffer should match the encrypted buffer from earlier in the test");
 			Assert.AreEqual(string.Join(",", buffer), string.Join(",", plain2), "decrypted buffer should match original buffer");
+		}
+		[TestMethod]
+		public void TestAsymmetricEncryptionNonPersistedKey()
+		{
+			byte[] plainBytes = ByteUtil.Utf8NoBOM.GetBytes("Secret String For Testing");
+
+			AsymmetricEncryption.GenerateNewKeys(out string publicKey, out string privateKey);
+
+			byte[] encryptedBytes = AsymmetricEncryption.EncryptWithKey(publicKey, plainBytes);
+			Assert.IsFalse(ByteUtil.ByteArraysMatch(plainBytes, encryptedBytes));
+
+			byte[] decryptedBytes = AsymmetricEncryption.DecryptWithKey(privateKey, encryptedBytes);
+			Assert.IsTrue(ByteUtil.ByteArraysMatch(plainBytes, decryptedBytes));
+
+			// Try encrypting with the private key (usually done with only the public key).
+			byte[] encryptedBytes2 = AsymmetricEncryption.EncryptWithKey(privateKey, plainBytes);
+			Assert.IsFalse(ByteUtil.ByteArraysMatch(plainBytes, encryptedBytes2));
+
+			// Try decrypting with the public key (should fail)
+			try
+			{
+				byte[] decryptedBytes2 = AsymmetricEncryption.DecryptWithKey(publicKey, encryptedBytes);
+				Assert.Fail("Expected exception when trying to decrypt with public key.");
+			}
+			catch { }
+
+			// Verify that private-key-encryption worked as intended
+			byte[] decryptedBytes3 = AsymmetricEncryption.DecryptWithKey(privateKey, encryptedBytes);
+			Assert.IsTrue(ByteUtil.ByteArraysMatch(plainBytes, decryptedBytes3));
+		}
+		private const string MachineKeyContainerName = "BpUtil Asymmetric Machine Test Key #1";
+		private const string UserKeyContainerName = "BpUtil Asymmetric Machine Test Key #1";
+		private void CleanupKeystores()
+		{
+			AsymmetricEncryption.DeletePublicKeyFromKeystore(Keystore.Machine, MachineKeyContainerName);
+			AsymmetricEncryption.DeletePublicKeyFromKeystore(Keystore.User, MachineKeyContainerName);
+			AsymmetricEncryption.DeletePublicKeyFromKeystore(Keystore.Machine, UserKeyContainerName);
+			AsymmetricEncryption.DeletePublicKeyFromKeystore(Keystore.User, UserKeyContainerName);
+		}
+		[TestMethod]
+		public void TestAsymmetricEncryptionWithMachineKeystore()
+		{
+			CleanupKeystores();
+			byte[] plainBytes = ByteUtil.Utf8NoBOM.GetBytes("Secret String For Testing");
+
+			try
+			{
+				byte[] encryptedBytes = AsymmetricEncryption.EncryptWithKeyFromKeystore(Keystore.Machine, MachineKeyContainerName, plainBytes);
+				Assert.IsFalse(ByteUtil.ByteArraysMatch(plainBytes, encryptedBytes));
+
+				byte[] decryptedBytes = AsymmetricEncryption.DecryptWithKeyFromKeystore(Keystore.Machine, MachineKeyContainerName, encryptedBytes);
+				Assert.IsTrue(ByteUtil.ByteArraysMatch(plainBytes, decryptedBytes));
+
+				// Key should be retrievable from correct keystore
+				AsymmetricEncryption.GenerateNewKeysInKeystore(Keystore.Machine, MachineKeyContainerName, out string publicKey);
+				string publicKeyLoaded = AsymmetricEncryption.GetPublicKeyFromKeystore(Keystore.Machine, MachineKeyContainerName);
+				Assert.AreEqual(publicKey, publicKeyLoaded);
+
+				// Key should NOT be retrievable from incorrect keystore
+				string publicKeyFromWrongKeystore = AsymmetricEncryption.GetPublicKeyFromKeystore(Keystore.User, MachineKeyContainerName);
+				Assert.IsNull(publicKeyFromWrongKeystore);
+
+				Assert.IsTrue(KeystoreContainsKeyContainer(Keystore.Machine, MachineKeyContainerName));
+				Assert.IsFalse(KeystoreContainsKeyContainer(Keystore.User, MachineKeyContainerName));
+
+				// Test encryption using exported public key.
+				byte[] encryptedBytes2 = AsymmetricEncryption.EncryptWithKey(publicKeyLoaded, plainBytes);
+				Assert.IsFalse(ByteUtil.ByteArraysMatch(plainBytes, encryptedBytes2));
+
+				byte[] decryptedBytes2 = AsymmetricEncryption.DecryptWithKeyFromKeystore(Keystore.Machine, MachineKeyContainerName, encryptedBytes2);
+				Assert.IsTrue(ByteUtil.ByteArraysMatch(plainBytes, decryptedBytes2));
+			}
+			finally
+			{
+				AsymmetricEncryption.DeletePublicKeyFromKeystore(Keystore.Machine, MachineKeyContainerName);
+
+				Assert.IsFalse(KeystoreContainsKeyContainer(Keystore.Machine, MachineKeyContainerName));
+				Assert.IsFalse(KeystoreContainsKeyContainer(Keystore.User, MachineKeyContainerName));
+			}
+		}
+		[TestMethod]
+		public void TestAsymmetricEncryptionWithUserKeystore()
+		{
+			CleanupKeystores();
+			byte[] plainBytes = ByteUtil.Utf8NoBOM.GetBytes("Secret String For Testing");
+
+			try
+			{
+				// Test encryption and decryption
+				byte[] encryptedBytes = AsymmetricEncryption.EncryptWithKeyFromKeystore(Keystore.User, UserKeyContainerName, plainBytes);
+				Assert.IsFalse(ByteUtil.ByteArraysMatch(plainBytes, encryptedBytes));
+
+				byte[] decryptedBytes = AsymmetricEncryption.DecryptWithKeyFromKeystore(Keystore.User, UserKeyContainerName, encryptedBytes);
+				Assert.IsTrue(ByteUtil.ByteArraysMatch(plainBytes, decryptedBytes));
+
+				// Key should be retrievable from correct keystore
+				AsymmetricEncryption.GenerateNewKeysInKeystore(Keystore.User, UserKeyContainerName, out string publicKey);
+				string publicKeyLoaded = AsymmetricEncryption.GetPublicKeyFromKeystore(Keystore.User, UserKeyContainerName);
+				Assert.AreEqual(publicKey, publicKeyLoaded);
+
+				// Key should NOT be retrievable from incorrect keystore
+				string publicKeyFromWrongKeystore = AsymmetricEncryption.GetPublicKeyFromKeystore(Keystore.Machine, UserKeyContainerName);
+				Assert.IsNull(publicKeyFromWrongKeystore);
+
+				Assert.IsFalse(KeystoreContainsKeyContainer(Keystore.Machine, MachineKeyContainerName));
+				Assert.IsTrue(KeystoreContainsKeyContainer(Keystore.User, MachineKeyContainerName));
+
+				// Test encryption using exported public key.
+				byte[] encryptedBytes2 = AsymmetricEncryption.EncryptWithKey(publicKeyLoaded, plainBytes);
+				Assert.IsFalse(ByteUtil.ByteArraysMatch(plainBytes, encryptedBytes2));
+
+				byte[] decryptedBytes2 = AsymmetricEncryption.DecryptWithKeyFromKeystore(Keystore.User, UserKeyContainerName, encryptedBytes2);
+				Assert.IsTrue(ByteUtil.ByteArraysMatch(plainBytes, decryptedBytes2));
+			}
+			finally
+			{
+				AsymmetricEncryption.DeletePublicKeyFromKeystore(Keystore.User, UserKeyContainerName);
+
+				Assert.IsFalse(KeystoreContainsKeyContainer(Keystore.Machine, MachineKeyContainerName));
+				Assert.IsFalse(KeystoreContainsKeyContainer(Keystore.User, MachineKeyContainerName));
+			}
+		}
+		/// <summary>
+		/// <para>If true, we determine if the keystore contains the key by simply trying to load it and seeing if it worked.</para>
+		/// <para>If false, we determine if the keystore contains the key by loading the names of all keys in the keystore. This is much slower.</para>
+		/// </summary>
+		bool fastKeystoreCheck = true;
+		/// <summary>
+		/// Returns true if the given keystore contains the given key container.
+		/// </summary>
+		/// <param name="useMachineKeyStore">Specify which keystore to look in.</param>
+		/// <param name="keyContainerName">Name of the key container to find.</param>
+		/// <returns></returns>
+		private bool KeystoreContainsKeyContainer(Keystore keystore, string keyContainerName)
+		{
+			if (fastKeystoreCheck)
+			{
+				return AsymmetricEncryption.GetPublicKeyFromKeystore(keystore, keyContainerName) != null;
+			}
+			else
+			{
+				List<string> containerNames = Helpers.IterateOverKeyContainers.GetKeyContainerNames(keystore == Keystore.Machine);
+				return containerNames.Contains(keyContainerName);
+			}
 		}
 	}
 }
