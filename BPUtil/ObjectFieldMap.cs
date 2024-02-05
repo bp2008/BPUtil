69caa67f6978f16efebcd4c4b7cb1ef3675f9e27
bp2008 <bp2008@users.noreply.github.com>
2024-02-05 09:15:27 -0700

    Added ObjectFieldMap class which scans an object's public field, property, and IList element values into a SortedList for easy comparison. Added unit tests for ObjectFieldMap class.

--- BPUtil/ObjectFieldMap.cs
+++ BPUtil/ObjectFieldMap.cs
@@ -9,155 +9,66 @@ using System.Threading.Tasks;
 namespace BPUtil
 {
 	/// <summary>
-	/// <para>This class scans all fields, properties, and IList indices of an object and its children to produce an iterable map of the object.</para>
-	/// <para>This class supports detecting changes in both fields and properties.</para>
-	/// <para>This class supports detecting additions, removals, and reordering of lists.</para>
-	/// <para>This class supports detecting changes to nested objects, which may be a different type than the parent object.</para>
-	/// <para>This class supports detecting changes to an IEnumerable and its elements, but does not support merging element-level changes.</para>
+	/// <para>This class scans all public instance-level fields and properties to produce a map of the object.  Members inheriting from IList are supported and their elements will be scanned into the ObjectFieldMap.</para>
+	/// <para>ObjectFieldMap is used by ObjectChangeReplay and ObjectThreeWayMerge classes.</para>
 	/// </summary>
 	public class ObjectFieldMap
 	{
 		/// <summary>
-		/// <para>The keys are strings which identify the hierarchy of field names, property names, or IList indices which one must traverse to find the object whose value is stored as the value.</para>
-		/// <para>Only primitive value types and strings are stored as the value.</para>
+		/// <para>The keys are strings which identify the field or property or IList index which contains the value.  Examples: "Name", "Child.Name", "Children[0].Name".</para>
+		/// <para>All values are strings or primitive value types or null.</para>
 		/// </summary>
 		private SortedList<string, object> map;
 
 		/// <summary>
-		/// Constructs an ObjectChangeHelper which stores the changes which the user made to the object and allows those changes to be replayed on a different object.
+		/// Constructs an ObjectFieldMap from the given object.
 		/// </summary>
-		/// <param name="original">The original object, before the user made changes.</param>
-		/// <param name="modified">The modified object, including the user's changes.</param>
-		public ObjectFieldMap(object original, object modified)
+		/// <param name="obj">The object which should be scanned.</param>
+		public ObjectFieldMap(object obj)
 		{
-			value = modified;
-			typeOfMember = modified?.GetType();
-			if (original == null || modified == null)
-			{
-				setValue = original != modified;
-				return;
-			}
-			Type originalType = original.GetType();
-			Type modifiedType = modified.GetType();
-			if (originalType != modifiedType)
-			{
-				setValue = true;
-				return;
-			}
-			//throw new ArgumentException("ObjectChangeHelper can't detect changes between original of type " + originalType.FullName + " and object of type " + modifiedType.FullName);
+			if (obj == null)
+				throw new ArgumentNullException(nameof(obj));
 
-			if (modifiedType.IsPrimitive || modifiedType == typeof(string))
-			{
-				if (!Object.Equals(original, modified))
-					setValue = true;
-				return;
-			}
+			Type objType = obj.GetType();
+			if ((objType.IsValueType && objType.IsPrimitive) || objType == typeof(string))
+				throw new ArgumentException("ObjectFieldMap can't be used directly on strings or primitive value types. Unsupported type: " + objType.FullName, nameof(obj));
 
-			// These objects represent more complex types.  Compare the fields and properties.
-			childChanges = new Dictionary<string, ObjectChangeHelper>();
-
-			// Check if the object is a list
-			if (original is IEnumerable originalEnumerable && modified is IEnumerable modifiedEnumerable)
+			map = new SortedList<string, object>();
+			Scan(obj, null);
+		}
+		private void Scan(object obj, string path)
+		{
+			Type objType = obj?.GetType();
+			if (obj == null || (objType.IsValueType && objType.IsPrimitive) || objType == typeof(string))
+				map[path] = obj;
+			else if (obj is IList list)
 			{
-				IEnumerator modifiedEnumerator = modifiedEnumerable.GetEnumerator();
-				foreach (object originalItem in originalEnumerable)
+				for (int i = 0; i < list.Count; i++)
 				{
-					object modifiedItem = modifiedEnumerator.Current;
-					if (!Object.Equals(originalItem, modifiedItem))
-					{
-						setValue = true;
-						return;
-					}
+					Scan(list[i], StringUtil.DeNullify(path) + "[" + i + "]");
 				}
 			}
-
-			// Check each field
-			foreach (FieldInfo field in originalType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+			else
 			{
-				object originalValue = field.GetValue(original);
-				object modifiedValue = field.GetValue(modified);
-
-				ObjectChangeHelper c = new ObjectChangeHelper(originalValue, modifiedValue);
-				if (!c.IsNoOp)
-					childChanges[field.Name] = c;
-			}
-
-			// Check each property
-			foreach (PropertyInfo prop in originalType.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
-			{
-				object originalValue = prop.GetValue(original);
-				object modifiedValue = prop.GetValue(modified);
-
-				ObjectChangeHelper c = new ObjectChangeHelper(originalValue, modifiedValue);
-				if (!c.IsNoOp)
-					childChanges[prop.Name] = c;
+				// Scan each field
+				foreach (FieldInfo field in objType.GetFields(BindingFlags.Public | BindingFlags.Instance))
+				{
+					object v = field.GetValue(obj);
+					Scan(v, ConcatPath(path, field.Name));
+				}
+				// Scan each property
+				foreach (PropertyInfo prop in objType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+				{
+					object v = prop.GetValue(obj);
+					Scan(v, ConcatPath(path, prop.Name));
+				}
 			}
 		}
-		/// <summary>
-		/// Replays the user's changes on the given object.  Returns true if a change was made.  False if no change was made.
-		/// </summary>
-		/// <param name="obj">An object that is the current state of a database record, upon which the user's changes should be applied.</param>
-		public bool Apply<T>(ref T obj)
+		private string ConcatPath(string pathRoot, string pathMore)
 		{
-			if (IsNoOp)
-				return false;
-			if (obj == null)
-			{
-				if (value == null)
-					return false;
-				obj = (T)value;
-				return true;
-			}
-			Type objType = obj.GetType();
-			if (typeOfMember != null)
-			{
-				// This ObjectChangeHelper is bound to a specific type.
-				if (objType != typeOfMember)
-					throw new Exception("ObjectChangeHelper cannot be applied to this object because its stored information is for another type. Stored type " + typeOfMember.Name + " can't apply to type " + typeof(T).Name);
-			}
-			if (setValue)
-			{
-				if (!Object.Equals(obj, value))
-				{
-					obj = (T)value;
-					return true;
-				}
-				return false;
-			}
-
-			bool madeChanges = false;
-			if (childChanges != null && childChanges.Count > 0)
-			{
-				foreach (KeyValuePair<string, ObjectChangeHelper> change in childChanges)
-				{
-					string memberName = change.Key;
-					MemberInfo myMember = objType.GetMember(memberName, MemberTypes.Property | MemberTypes.Field, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).SingleOrDefault();
-					if (myMember == null)
-						continue;
-
-					ObjectChangeHelper changeHelper = change.Value;
-					object child;
-					if (myMember is FieldInfo myField)
-					{
-						child = myField.GetValue(obj);
-						if (changeHelper.Apply(ref child))
-						{
-							myField.SetValue(obj, child);
-							madeChanges = true;
-						}
-					}
-					else if (myMember is PropertyInfo myProp)
-					{
-						child = myProp.GetValue(obj);
-						if (changeHelper.Apply(ref child))
-						{
-							myProp.SetValue(obj, child);
-							madeChanges = true;
-						}
-					}
-				}
-			}
-			return madeChanges;
+			if (pathRoot != null)
+				return pathRoot + "." + pathMore;
+			return pathMore;
 		}
 		/// <summary>
 		/// Returns a string describing the hierarchy of changes which are stored in this object.
@@ -165,34 +76,23 @@ namespace BPUtil
 		/// <returns></returns>
 		public override string ToString()
 		{
-			return ToString(0);
-		}
-		private string Indent(int indents)
-		{
 			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < indents; i++)
-				sb.Append("  ");
-			return sb.ToString();
-		}
-		private string ToString(int indents)
-		{
-			if (IsNoOp)
-				return "No-Op";
-			if (setValue)
-				return Indent(indents) + (value == null ? "null" : value.ToString());
-			else if (childChanges != null && childChanges.Count > 0)
+			foreach (KeyValuePair<string, object> item in map)
 			{
-				StringBuilder sb = new StringBuilder();
-				foreach (KeyValuePair<string, ObjectChangeHelper> change in childChanges)
-				{
-					sb.Append(Indent(indents));
-					sb.AppendLine(change.Key + " -> ");
-					sb.AppendLine(change.Value.ToString(indents + 1));
-				}
-				return sb.ToString();
+				if (sb.Length > 0)
+					sb.AppendLine();
+				sb.Append(item.Key);
+				sb.Append(" = ");
+				if (item.Value == null)
+					sb.Append("null");
+				else if (item.Value is string)
+					sb.Append('"').Append(item.Value).Append('"');
+				else if (item.Value is char)
+					sb.Append('\'').Append(item.Value).Append('\'');
+				else
+					sb.Append(item.Value.ToString());
 			}
-			else
-				return "";
+			return sb.ToString();
 		}
 	}
 }
---
+++ UnitTests/TestObjectFieldMap.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using BPUtil;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class TestObjectFieldMap
+	{
+		[TestMethod]
+		public void TestObjectFieldMap_ArgumentExceptions()
+		{
+			try
+			{
+				new ObjectFieldMap(null);
+				Assert.Fail("Expected exception from passing in null");
+			}
+			catch { }
+			try
+			{
+				new ObjectFieldMap("str");
+				Assert.Fail("Expected exception from passing in string");
+			}
+			catch { }
+			try
+			{
+				new ObjectFieldMap(1);
+				Assert.Fail("Expected exception from passing in int");
+			}
+			catch { }
+		}
+		[TestMethod]
+		public void TestObjectFieldMap_List()
+		{
+			List<int> list = new List<int>();
+			list.Add(2);
+			list.Add(4);
+			list.Add(8);
+			ObjectFieldMap map = new ObjectFieldMap(list);
+			Assert.AreEqual("[0] = 2" + Environment.NewLine
+				+ "[1] = 4" + Environment.NewLine
+				+ "[2] = 8", map.ToString());
+		}
+		[TestMethod]
+		public void TestObjectFieldMap_SimpleObject()
+		{
+			SO obj = new SO(1, "s");
+			ObjectFieldMap map = new ObjectFieldMap(obj);
+			Assert.AreEqual("A = 1" + Environment.NewLine + "B = \"s\"", map.ToString());
+		}
+		[TestMethod]
+		public void TestObjectFieldMap_SimpleObject2()
+		{
+			SO2 obj = new SO2(' ');
+			ObjectFieldMap map = new ObjectFieldMap(obj);
+			Assert.AreEqual("c = ' '", map.ToString());
+
+			obj = new SO2((char)0);
+			map = new ObjectFieldMap(obj);
+			Assert.AreEqual("c = '\0'", map.ToString());
+		}
+		[TestMethod]
+		public void TestObjectFieldMap_ComplexObject1()
+		{
+			CO obj = new CO(new SO(1, "s"), null, "&");
+			ObjectFieldMap map = new ObjectFieldMap(obj);
+			Assert.AreEqual("X.A = 1" + Environment.NewLine
+				+ "X.B = \"s\"" + Environment.NewLine
+				+ "Y = null" + Environment.NewLine
+				+ "Z = \"&\"", map.ToString());
+		}
+		[TestMethod]
+		public void TestObjectFieldMap_ComplexObject2()
+		{
+			CO obj = new CO(new SO(1, "s"), null, null);
+			ObjectFieldMap map = new ObjectFieldMap(obj);
+			Assert.AreEqual("X.A = 1" + Environment.NewLine
+				+ "X.B = \"s\"" + Environment.NewLine
+				+ "Y = null" + Environment.NewLine
+				+ "Z = null", map.ToString());
+		}
+		[TestMethod]
+		public void TestObjectFieldMap_ComplexObject3()
+		{
+			CO obj = new CO(new SO(1, "s"), new SO(2, null), "m");
+			ObjectFieldMap map = new ObjectFieldMap(obj);
+			Assert.AreEqual("X.A = 1" + Environment.NewLine
+				+ "X.B = \"s\"" + Environment.NewLine
+				+ "Y.A = 2" + Environment.NewLine
+				+ "Y.B = null" + Environment.NewLine
+				+ "Z = \"m\"", map.ToString());
+		}
+		[TestMethod]
+		public void TestObjectFieldMap_ComplexObject4()
+		{
+			CO4 obj = new CO4(new SO(-1, ""), new double[] { 0.1, -0.9, double.MaxValue }, new string[] { "First", "", null });
+			ObjectFieldMap map = new ObjectFieldMap(obj);
+			Assert.AreEqual("X.A = -1" + Environment.NewLine
+				+ "X.B = \"\"" + Environment.NewLine
+				+ "Y[0] = 0.1" + Environment.NewLine
+				+ "Y[1] = -0.9" + Environment.NewLine
+				+ "Y[2] = 1.79769313486232E+308" + Environment.NewLine
+				+ "Z[0] = \"First\"" + Environment.NewLine
+				+ "Z[1] = \"\"" + Environment.NewLine
+				+ "Z[2] = null", map.ToString());
+		}
+		class SO
+		{
+			public int A { get; set; }
+			public string B;
+			public SO(int a, string b) { A = a; B = b; }
+		}
+		class SO2
+		{
+			public char c;
+			public SO2(char c) { this.c = c; }
+		}
+		class CO
+		{
+			public SO X { get; set; }
+			public SO Y;
+			public string Z;
+			public CO(SO x, SO y, string z) { X = x; Y = y; Z = z; }
+		}
+		class CO4
+		{
+			public SO X { get; set; }
+			public double[] Y;
+			public string[] Z;
+			public CO4(SO x, double[] y, string[] z) { X = x; Y = y; Z = z; }
+		}
+	}
+}
