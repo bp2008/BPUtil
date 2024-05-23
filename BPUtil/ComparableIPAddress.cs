7aef47548180cf9ac3a3c28dade99c02bb979bd3
bp2008 <bp2008@users.noreply.github.com>
2024-05-23 09:26:23 -0600

    Revised IPRangeLite and ComparableIPAddress classes to improve usability and documentation.

--- BPUtil/ComparableIPAddress.cs
+++ BPUtil/ComparableIPAddress.cs
@@ -12,24 +12,51 @@ namespace BPUtil
 	/// </summary>
 	public class ComparableIPAddress : IPAddress, IComparable, IComparable<IPAddress>, IComparable<string>, IComparable<ComparableIPAddress>
 	{
+		/// <summary>
+		/// Length of this IP address, in bytes.
+		/// </summary>
 		public int byteLength { get; private set; }
 		protected ulong mostSignificantBytes = 0;
 		protected ulong leastSignificantBytes = 0;
+		/// <summary>
+		/// Constructs a ComparableIPAddress from an IPAddress.
+		/// </summary>
+		/// <param name="address">IPAddress</param>
+		public ComparableIPAddress(IPAddress address) : this(address.GetAddressBytes())
+		{
+			if (address?.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+				this.ScopeId = address.ScopeId;
+		}
+		/// <summary>
+		/// Constructs a ComparableIPAddress from a byte array containing the address bytes.
+		/// </summary>
+		/// <param name="address">The byte array value of the IP address.</param>
 		public ComparableIPAddress(byte[] address) : base(address)
 		{
 			Initialize(address);
 		}
-
-		public ComparableIPAddress(Int64 address) : base(address)
+		/// <summary>
+		/// Initializes a new instance of the ComparableIPAddress class with the address specified as an <see cref="long">.
+		/// </summary>
+		/// <param name="address">The <see cref="long"> value of the IP address. For example, the value 0x2414188f in big-endian format would be the IP address "143.24.20.36".</param>
+		public ComparableIPAddress(long address) : base(address)
 		{
 			Initialize(null);
 		}
-
+		/// <summary>
+		/// (for IPv6) Initializes a new instance of the ComparableIPAddress class with the address specified as a <see cref="byte"/> array and the specified scope identifier.
+		/// </summary>
+		/// <param name="address">The byte array value of the IP address.</param>
+		/// <param name="scopeid">The long value of the IPv6 scope identifier.</param>
 		public ComparableIPAddress(byte[] address, Int64 scopeid) : base(address, scopeid)
 		{
 			Initialize(address);
 		}
-
+		/// <summary>
+		/// Initializes the two <see cref="ulong"/> fields <see cref="mostSignificantBytes"/> and <see cref="leastSignificantBytes"/> 
+		/// which make this ComparableIPAddress able to compare with other ComparableIPAddress very slightly faster.
+		/// </summary>
+		/// <param name="address"></param>
 		private void Initialize(byte[] address)
 		{
 			if (address == null)
@@ -42,10 +69,14 @@ namespace BPUtil
 				else
 					leastSignificantBytes |= ((ulong)address[i] << (120 - (i * 8)));
 			}
+			//string binary1 = Convert.ToString((long)mostSignificantBytes, 2).PadLeft(64, '0') + Convert.ToString((long)leastSignificantBytes, 2).PadLeft(64, '0');
+			//string binary2 = string.Join("", address.Select(b => Convert.ToString(b, 2).PadLeft(8, '0'))).PadRight(128, '0');
+			//if (binary1 != binary2)
+			//	throw new Exception("Binary loading failed: \r\n" + binary1 + "\r\n" + binary2);
 		}
 
 		/// <summary>
-		/// Parses the specified string into a ComparableIPAddress, possibly returning null if the string does not represent a valid IP address.
+		/// Parses the specified string into a ComparableIPAddress, returning null if the string does not represent a valid IP address.
 		/// </summary>
 		/// <param name="str">String that represents an IP address.</param>
 		/// <returns></returns>
@@ -59,7 +90,7 @@ namespace BPUtil
 		/// <summary>
 		/// Compares this address with another address.
 		/// </summary>
-		/// <param name="obj">Other address.</param>
+		/// <param name="other">Other address.</param>
 		/// <returns></returns>
 		public int CompareTo(object obj)
 		{
--- BPUtil/IpRangeLite.cs
+++ BPUtil/IpRangeLite.cs
@@ -17,50 +17,56 @@ namespace BPUtil
 		/// </summary>
 		public readonly ComparableIPAddress high;
 		/// <summary>
-		/// True if the <see cref="low"/> and <see cref="high"/> addresses are valid IP addresses in the same address family.
+		/// Constructs an IpRangeLite from two IP addresses.
 		/// </summary>
-		public readonly bool isValid = false;
-
-		/// <summary>
-		/// Constructs an IpRangeLite from any two IP addresses.
-		/// </summary>
-		/// <param name="ip1"></param>
-		/// <param name="ip2"></param>
+		/// <param name="ip1">IP Address</param>
+		/// <param name="ip2">IP Address</param>
 		public IpRangeLite(string ip1, string ip2)
+			: this(ComparableIPAddress.Parse(ip1), ComparableIPAddress.Parse(ip2)) { }
+		/// <summary>
+		/// Constructs an IpRangeLite from two IP addresses.
+		/// </summary>
+		/// <param name="ip1">IP Address</param>
+		/// <param name="ip2">IP Address</param>
+		public IpRangeLite(IPAddress ip1, IPAddress ip2) : this(new ComparableIPAddress(ip1), new ComparableIPAddress(ip2)) { }
+		/// <summary>
+		/// Constructs an IpRangeLite from two IP addresses.
+		/// </summary>
+		/// <param name="ip1">IP Address</param>
+		/// <param name="ip2">IP Address</param>
+		public IpRangeLite(ComparableIPAddress ip1, ComparableIPAddress ip2)
 		{
-			low = ComparableIPAddress.Parse(ip1);
-			high = ComparableIPAddress.Parse(ip2);
-			if (low == null || high == null || low.byteLength != high.byteLength)
-				return;
+			low = ip1;
+			high = ip2;
+			if (low == null)
+				throw new ArgumentNullException(nameof(low));
+			if (high == null)
+				throw new ArgumentNullException(nameof(high));
+			if (low.byteLength != high.byteLength)
+				throw new ArgumentException("The given IP addresses are not in the same address family.");
 			if (low.CompareTo(high) > 0)
 			{
 				ComparableIPAddress tmp = low;
 				low = high;
 				high = tmp;
 			}
-			isValid = true;
 		}
-
 		/// <summary>
 		/// Returns true if the specified IP address is contained within the IP range.
 		/// </summary>
 		/// <param name="address">IP Address</param>
-		/// <returns></returns>
+		/// <returns>true if the specified IP address is contained within the IP range.</returns>
 		public bool Contains(string address)
 		{
-			if (!isValid)
-				return false;
 			return Contains(ComparableIPAddress.Parse(address));
 		}
 		/// <summary>
 		/// Returns true if the specified IP address is contained within the IP range.
 		/// </summary>
 		/// <param name="address">IP Address</param>
-		/// <returns></returns>
+		/// <returns>true if the specified IP address is contained within the IP range.</returns>
 		public bool Contains(IPAddress address)
 		{
-			if (!isValid)
-				return false;
 			if (address == null)
 				return false;
 			return low.CompareTo(address) <= 0 && high.CompareTo(address) >= 0;
