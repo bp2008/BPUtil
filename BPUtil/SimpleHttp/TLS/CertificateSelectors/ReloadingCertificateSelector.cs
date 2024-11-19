d78d2a197fb02121919b910f75f3412e7ad5eb70
bp2008 <bp2008@users.noreply.github.com>
2024-11-19 13:55:29 -0700

    Added a new certificate selector that periodically reloads from disk so that certificate renewals do not require the applicaiton to be restarted. Added a helper class `CertificateStoreUpdater` that puts intermediate certificates into the system's certificate store so that SslStream can include them in TLS negotiation.

---
+++ BPUtil/SimpleHttp/TLS/CertificateSelectors/ReloadingCertificateSelector.cs
@@ -0,0 +1,192 @@
+﻿using BPUtil.SimpleHttp.TLS;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BPUtil.SimpleHttp
+{
+	/// <summary>
+	/// An object containing the path of a `.pfx` file and an optional password.
+	/// </summary>
+	public class CertificatePfxInfo
+	{
+		/// <summary>
+		/// Path to a `.pfx` file.
+		/// </summary>
+		public readonly string FilePath;
+		/// <summary>
+		/// Password for the `.pfx` file, or null.
+		/// </summary>
+		public readonly string Password;
+		/// <summary>
+		/// Constructs a CertificatePfxInfo from arguments.
+		/// </summary>
+		/// <param name="filePath">Path to a `.pfx` file.</param>
+		/// <param name="password">Password for the `.pfx` file, or null.</param>
+		public CertificatePfxInfo(string filePath, string password)
+		{
+			FilePath = filePath;
+			Password = password;
+		}
+		/// <summary>
+		/// Determines if this CertificatePfxInfo is equivalent to another CertificatePfxInfo.
+		/// </summary>
+		/// <param name="obj">Another object to compare with.</param>
+		/// <returns></returns>
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+				return false;
+			else if (obj == this)
+				return true;
+			else if (obj is CertificatePfxInfo other)
+				return this.FilePath == other.FilePath && this.Password == other.Password;
+			else
+				return false;
+		}
+		/// <inheritdoc/>
+		public override int GetHashCode()
+		{
+			return FilePath.GetHashCode() ^ Password.GetHashCode();
+		}
+		/// <inheritdoc/>
+		public override string ToString()
+		{
+			return FilePath + (Password != null ? " (password protected)" : "");
+		}
+	}
+	/// <summary>
+	/// Provides simple SSL certificate selection capabilities with periodic reloading from disk.
+	/// </summary>
+	public class ReloadingCertificateSelector : ICertificateSelector
+	{
+		private readonly Func<CertificatePfxInfo> certificateInfoProvider;
+		private CachedCertificate cachedCertificate;
+		private CertificatePfxInfo lastCertificatePfxInfo = null;
+		private object myLock = new object();
+		private Cooldown checkLastWriteTimeCd = new Cooldown(10000);
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ReloadingCertificateSelector"/> class.
+		/// </summary>
+		/// <param name="certificateInfoProvider">A function that returns a CertificatePfxInfo containing the path of the certificate and an optional password.  This will be called every time the web server needs to negotiate a TLS connection, so it should be very efficient.</param>
+		public ReloadingCertificateSelector(Func<CertificatePfxInfo> certificateInfoProvider)
+		{
+			this.certificateInfoProvider = certificateInfoProvider ?? throw new ArgumentNullException(nameof(certificateInfoProvider));
+		}
+
+		/// <summary>
+		/// Returns an X509Certificate or null if no certificate is available (the connection will be closed).
+		/// </summary>
+		/// <param name="p">The HttpProcessor instance in its current early state of processing. Many fields have not been initialized yet.</param>
+		/// <param name="serverName">The server name as indicated by ServerNameIndication. May be null or empty. May be omitted if the configured certificate resolver is known to ignore this value.</param>
+		/// <returns>An X509Certificate or null.</returns>
+		public Task<X509Certificate> GetCertificate(HttpProcessor p, string serverName)
+		{
+			if (ShouldReloadCertificate(false))
+			{
+				lock (myLock)
+				{
+					if (ShouldReloadCertificate(true))
+						ReloadCertificate();
+				}
+			}
+			return Task.FromResult<X509Certificate>(cachedCertificate?.Certificate);
+		}
+
+		/// <summary>
+		/// Returns a special X509Certificate (or null) in order to complete "acme-tls/1" or "TLS-ALPN-01" validation.
+		/// </summary>
+		/// <param name="p">The HttpProcessor instance in its current early state of processing. Many fields have not been initialized yet.</param>
+		/// <param name="serverName">The server name as indicated by ServerNameIndication. This is a required parameter and should not be null or empty.</param>
+		/// <returns>An X509Certificate or null.</returns>
+		public Task<X509Certificate> GetAcmeTls1Certificate(HttpProcessor p, string serverName)
+		{
+			return Task.FromResult<X509Certificate>(null);
+		}
+
+		private bool ShouldReloadCertificate(bool isRecheck)
+		{
+			CachedCertificate cc = cachedCertificate;
+			if (cc == null)
+				return true; // The certificate hasn't been loaded yet.
+			if (cc.IsExpiringSoon)
+				return true; // The certificate is expired or expiring very soon.
+			if (cc.Age > TimeSpan.FromDays(1))
+				return true; // We haven't reloaded the certificate in over a day.
+
+			CertificatePfxInfo cpi = certificateInfoProvider();
+			if (!cpi.Equals(lastCertificatePfxInfo))
+				return true;
+
+			if (isRecheck || checkLastWriteTimeCd.Consume())
+			{
+				DateTime lastModifiedUtc = File.GetLastWriteTimeUtc(cpi.FilePath);
+				if (cc.FileLastModifiedUTC != lastModifiedUtc)
+					return true;
+			}
+
+			return false;
+		}
+
+		private void ReloadCertificate()
+		{
+			CertificatePfxInfo cpi = certificateInfoProvider();
+			FileInfo fi = new FileInfo(cpi.FilePath);
+			if (fi.Exists)
+			{
+				CachedCertificate cc = new CachedCertificate(fi, cpi.Password);
+				try
+				{
+					CertificateStoreUpdater.EnsureIntermediateCertificatesAreInStore(fi.FullName, cpi.Password);
+				}
+				catch (Exception ex)
+				{
+					Logger.Debug(ex, "Unable to save intermediate certificates to \"Intermediate Certificate Authorites\" store.");
+					return;
+				}
+				lastCertificatePfxInfo = cpi;
+				cachedCertificate = cc;
+			}
+		}
+	}
+	/// <summary>
+	/// An object storing a cached certificate.
+	/// </summary>
+	public class CachedCertificate
+	{
+		/// <summary>
+		/// The cached certificate.
+		/// </summary>
+		public X509Certificate2 Certificate { get; private set; }
+		/// <summary>
+		/// A Stopwatch containing the age of the cached certificate.
+		/// </summary>
+		private Stopwatch age = Stopwatch.StartNew();
+		/// <summary>
+		/// The age of this cached certificate instance.
+		/// </summary>
+		public TimeSpan Age => age.Elapsed;
+		/// <summary>
+		/// The Last Modified timestamp of the certificate file, in UTC.
+		/// </summary>
+		public DateTime FileLastModifiedUTC { get; private set; }
+		/// <summary>
+		/// True if the certificate is expired or expiring within the next 1 hour.
+		/// </summary>
+		public bool IsExpiringSoon => Certificate.NotAfter < DateTime.Now.AddHours(1);
+		/// <summary>
+		/// Constructs a new CachedCertificate by reading the certificate from a file.
+		/// </summary>
+		/// <param name="fi">FileInfo pointing at a pfx file.</param>
+		/// <param name="password">Pfx file password or null.</param>
+		public CachedCertificate(FileInfo fi, string password)
+		{
+			Certificate = new X509Certificate2(fi.FullName, password);
+			FileLastModifiedUTC = fi.LastWriteTimeUtc;
+		}
+	}
+}
---
+++ BPUtil/SimpleHttp/TLS/CertificateStoreUpdater.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPUtil.SimpleHttp.TLS
+{
+	/// <summary>
+	/// <para>A class which ensures intermediate certificates are in the OS's certificate store so that SslStream will serve them to clients.</para>
+	/// <para>As of 2024-11-19, I've tested and it seems it is not actually necessary to add LetsEncrypt's intermediate servers to the certificate store for some reason, and SslStream somehow finds them anyway.  However if you use a private certificate authority, SslStream requires it to be found in the OS certificate store.</para>
+	/// </summary>
+	public static class CertificateStoreUpdater
+	{
+		private static Dictionary<string, bool> ThumbprintsAlreadyAdded = new Dictionary<string, bool>();
+		/// <summary>
+		/// Adds all intermediate certificates from the given pfx file to the local machine's "Intermediate Certificate Authorities" store (skips those that already exist in it).
+		/// </summary>
+		/// <param name="pfxFilePath">Path to a pfx file containing intermediate certificates.</param>
+		/// <param name="pfxPassword">Password for the pfx file.  Null if the file is not password-protected.</param>
+		public static void EnsureIntermediateCertificatesAreInStore(string pfxFilePath, string pfxPassword = null)
+		{
+			// Load the .pfx file into an X509Certificate2Collection
+			X509Certificate2Collection certificateCollection = new X509Certificate2Collection();
+			certificateCollection.Import(pfxFilePath, pfxPassword, X509KeyStorageFlags.DefaultKeySet);
+			EnsureIntermediateCertificatesAreInStore(certificateCollection);
+		}
+		/// <summary>
+		/// Adds all intermediate certificates from the given pfx file to the local machine's "Intermediate Certificate Authorities" store (skips those that already exist in it).
+		/// </summary>
+		/// <param name="pfxFileData">Binary content of a pfx file containing intermediate certificates.</param>
+		/// <param name="pfxPassword">Password for the pfx file.  Null if the file is not password-protected.</param>
+		public static void EnsureIntermediateCertificatesAreInStore(byte[] pfxFileData, string pfxPassword = null)
+		{
+			// Load the .pfx file into an X509Certificate2Collection
+			X509Certificate2Collection certificateCollection = new X509Certificate2Collection();
+			certificateCollection.Import(pfxFileData, pfxPassword, X509KeyStorageFlags.DefaultKeySet);
+			EnsureIntermediateCertificatesAreInStore(certificateCollection);
+		}
+		/// <summary>
+		/// <para>Adds all intermediate certificates from the given pfx file to the local machine's "Intermediate Certificate Authorities" store (skips those that already exist in it).</para>
+		/// </summary>
+		/// <param name="certificateCollection">An X509Certificate2Collection containing a leaf and zero or more intermediate (issuer) certificates.</param>
+		public static void EnsureIntermediateCertificatesAreInStore(X509Certificate2Collection certificateCollection)
+		{
+			// There need to be at least 2 certificates in the pfx file in order for there to be any intermediates that we care about.
+			// Otherwise a self-signed certificate would be its own issuer and would get added to the store for no purpose.
+			if (certificateCollection.Count < 2)
+				return;
+
+			// Collect all the issuers' subjects
+			HashSet<string> issuerSubjects = new HashSet<string>();
+			foreach (X509Certificate2 certificate in certificateCollection)
+				issuerSubjects.Add(certificate.Issuer);
+
+			// We only want to add to the store any certificates that were the issuer of another certificate in the same collection.
+			List<X509Certificate2> intermediates = new List<X509Certificate2>();
+			foreach (X509Certificate2 certificate in certificateCollection)
+				if (issuerSubjects.Contains(certificate.Subject))
+					intermediates.Add(certificate);
+
+			intermediates.RemoveAll(cert => ThumbprintsAlreadyAdded.ContainsKey(cert.Thumbprint));
+
+			if (Platform.IsUnix())
+			{
+				// Attempting to add to LocalMachine yields:
+				// System.PlatformNotSupportedException: Unix LocalMachine X509Stores are read-only for all users.
+				EnsureCertificatesAreInStore(intermediates, StoreLocation.CurrentUser);
+			}
+			else
+			{
+				try
+				{
+					EnsureCertificatesAreInStore(intermediates, StoreLocation.LocalMachine);
+				}
+				catch (Exception)
+				{
+					EnsureCertificatesAreInStore(intermediates, StoreLocation.CurrentUser);
+				}
+			}
+		}
+
+		private static void EnsureCertificatesAreInStore(List<X509Certificate2> certificates, StoreLocation storeLocation)
+		{
+			if (certificates.Count == 0)
+				return;
+
+#if NETFRAMEWORK
+			X509Store store = new X509Store(StoreName.CertificateAuthority, storeLocation);
+			try
+#else
+			using (X509Store store = new X509Store(StoreName.CertificateAuthority, storeLocation))
+#endif
+			{
+				store.Open(OpenFlags.ReadWrite);
+
+				foreach (X509Certificate2 certificate in certificates)
+				{
+					// Check if the certificate is already in the store
+					X509Certificate2Collection existingCertificates = store.Certificates.Find(X509FindType.FindByThumbprint, certificate.Thumbprint, false);
+					if (existingCertificates.Count == 0)
+					{
+						// Add the certificate to the store if it's not already there
+						store.Add(certificate);
+					}
+					ThumbprintsAlreadyAdded[certificate.Thumbprint] = true;
+				}
+			}
+#if NETFRAMEWORK
+			finally
+			{
+				store.Close();
+			}
+#endif
+		}
+	}
+}
