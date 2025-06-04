b64949cc856640d59e6c14fc1601c77f661e1fb2
bp2008 <bp2008@users.noreply.github.com>
2025-06-04 12:32:48 -0600

    DnsHelper now includes the hostname in all exceptions.

--- BPUtil/DnsHelper.cs
+++ BPUtil/DnsHelper.cs
@@ -27,42 +27,49 @@ namespace BPUtil
 		/// <returns>A task that represents the asynchronous operation. The value of the TResult parameter contains an IPAddress for the host that is specified by the hostNameOrAddress parameter.</returns>
 		public static async Task<IPAddress> GetHostAddressAsync(string hostNameOrAddress, CancellationToken cancellationToken = default)
 		{
-			// Cleanup expired cache entries at most once per minute
-			DateTime now = DateTime.UtcNow;
-			if (cacheCleanupCooldown.Consume())
+			try
 			{
-				foreach (KeyValuePair<string, DnsCacheEntry> kvp in cache)
+				// Cleanup expired cache entries at most once per minute
+				DateTime now = DateTime.UtcNow;
+				if (cacheCleanupCooldown.Consume())
 				{
-					if (kvp.Value.Expired)
-						cache.TryRemove(kvp.Key, out _);
+					foreach (KeyValuePair<string, DnsCacheEntry> kvp in cache)
+					{
+						if (kvp.Value.Expired)
+							cache.TryRemove(kvp.Key, out _);
+					}
 				}
-			}
 
-			IPAddress ip;
-			if (IPAddress.TryParse(hostNameOrAddress, out ip))
-				return ip;
+				IPAddress ip;
+				if (IPAddress.TryParse(hostNameOrAddress, out ip))
+					return ip;
 
-			if (cache.TryGetValue(hostNameOrAddress, out DnsCacheEntry cacheEntry) && !cacheEntry.Expired)
-				return cacheEntry.Address;
+				if (cache.TryGetValue(hostNameOrAddress, out DnsCacheEntry cacheEntry) && !cacheEntry.Expired)
+					return cacheEntry.Address;
 
 #if NET6_0
 			IPAddress[] addresses = await Dns.GetHostAddressesAsync(hostNameOrAddress, cancellationToken).ConfigureAwait(false);
 #else
-			IPAddress[] addresses = await Dns.GetHostAddressesAsync(hostNameOrAddress).ConfigureAwait(false);
+				IPAddress[] addresses = await Dns.GetHostAddressesAsync(hostNameOrAddress).ConfigureAwait(false);
 #endif
 
-			ip = addresses.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
-			if (ip == null)
-				ip = addresses.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetworkV6);
-			if (ip == null)
-			{
-				cache.TryRemove(hostNameOrAddress, out DnsCacheEntry ignored);
-				throw new ApplicationException("Unable to resolve IP address for hostname \"" + hostNameOrAddress + "\"");
-			}
+				ip = addresses.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
+				if (ip == null)
+					ip = addresses.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetworkV6);
+				if (ip == null)
+				{
+					cache.TryRemove(hostNameOrAddress, out DnsCacheEntry ignored);
+					throw new ApplicationException("Unable to resolve IPv4 or IPv6 address.");
+				}
 
-			int ttl = ip.AddressFamily == AddressFamily.InterNetworkV6 ? 120 : 30;
-			cache[hostNameOrAddress] = new DnsCacheEntry(ip, ttl);
-			return ip;
+				int ttl = ip.AddressFamily == AddressFamily.InterNetworkV6 ? 120 : 30;
+				cache[hostNameOrAddress] = new DnsCacheEntry(ip, ttl);
+				return ip;
+			}
+			catch (Exception ex)
+			{
+				throw new ApplicationException("Failed to resolve \"" + hostNameOrAddress + "\".", ex);
+			}
 		}
 
 		class DnsCacheEntry
