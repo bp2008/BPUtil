b2d432abd5dcef1525affbfd702915e24dcb43e7
bp2008 <bp2008@users.noreply.github.com>
2025-11-05 10:37:27 -0700

    Added EnumExtensions class and unit tests.

---
+++ BPUtil/EnumExtensions.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPUtil
+{
+	public static class EnumExtensions
+	{
+		/// <summary>
+		/// Gets this enum value as a string, preferring to use the optional value provided by <see cref="DescriptionAttribute"/> if available.  For some of our enums, the Description attribute contains a string from our database so ToDescriptionString() returns the string as it is found in the database.
+		/// </summary>
+		/// <param name="val">Enum value which may or may not have a Description annotation.</param>
+		/// <returns></returns>
+		public static string ToDescriptionString(this Enum val)
+		{
+			DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+			   .GetType()
+			   .GetField(val.ToString())
+			   ?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+			return attributes != null && attributes.Length > 0 ? attributes[0].Description : val.ToString();
+		}
+		/// <summary>
+		/// Gets the value of the <see cref="DescriptionAttribute"/> assigned to this enum value, or null if none is assigned.
+		/// </summary>
+		/// <param name="val">Enum value which may or may not have a Description annotation.</param>
+		/// <returns></returns>
+		public static string GetDescriptionString(this Enum val)
+		{
+			DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+			   .GetType()
+			   .GetField(val.ToString())
+			   ?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+			return attributes != null && attributes.Length > 0 ? attributes[0].Description : null;
+		}
+		/// <summary>
+		/// Gets a collection containing the custom attributes of the specified type that are declared on the specified enum value.
+		/// 
+		/// </summary>
+		/// <param name="val">Enum value which may or may not have annotations of type <typeparamref name="T"/>.</param>
+		/// <returns></returns>
+		public static IEnumerable<T> GetCustomAttributes<T>(this Enum val)
+		{
+			FieldInfo fieldInfo = val.GetType().GetField(val.ToString());
+			if (fieldInfo == null)
+				return Enumerable.Empty<T>();
+			else
+				return fieldInfo.GetCustomAttributes(typeof(T), false).Cast<T>();
+		}
+		/// <summary>
+		/// <para>Finds the enum value matching the specified string, returning true if found.  Searches the Description attributes first, then falls back to using standard <c>Enum.TryParse()</c>.</para>
+		/// <para>This operation is case-sensitive.</para>
+		/// </summary>
+		/// <typeparam name="T">Type to match.</typeparam>
+		/// <param name="description">Description string to match.</param>
+		/// <param name="enumValue">(Output) the matched enum value.</param>
+		/// <returns>True if the match is successful, false otherwise.</returns>
+		public static bool TryParseWithDescription<T>(string description, out T enumValue) where T : struct, Enum
+		{
+			return TryParseWithDescription(description, false, out enumValue);
+		}
+		/// <summary>
+		/// Finds the enum value matching the specified string, returning true if found.  Searches the Description attributes first, then falls back to using standard <c>Enum.TryParse()</c>.
+		/// </summary>
+		/// <typeparam name="T">Type to match.</typeparam>
+		/// <param name="description">Description string to match.</param>
+		/// <param name="ignoreCase">true to ignore case; false to regard case.</param>
+		/// <param name="enumValue">(Output) the matched enum value.</param>
+		/// <returns>True if the match is successful, false otherwise.</returns>
+		public static bool TryParseWithDescription<T>(string description, bool ignoreCase, out T enumValue) where T : struct, Enum
+		{
+			if (description == null)
+				throw new ArgumentNullException(nameof(description));
+			if (ignoreCase)
+			{
+				foreach (T val in Enum.GetValues(typeof(T)))
+				{
+					if (description.IEquals(val.GetDescriptionString()))
+					{
+						enumValue = val;
+						return true;
+					}
+				}
+			}
+			else
+			{
+				foreach (T val in Enum.GetValues(typeof(T)))
+				{
+					if (description.Equals(val.GetDescriptionString()))
+					{
+						enumValue = val;
+						return true;
+					}
+				}
+			}
+			return Enum.TryParse(description, ignoreCase, out enumValue);
+		}
+
+		/// <summary>
+		/// <para>Converts the string representation of the name or numeric value of one or more enumerated constants to an equivalent enumerated object.</para>
+		/// <para>This operation is case-sensitive.</para>
+		/// <para>If the given string is invalid for the given enumeration type, an exception is thrown.</para>
+		/// </summary>
+		/// <typeparam name="T">An enumeration type.</typeparam>
+		/// <param name="value">A string containing the name or value to convert.</param>
+		/// <returns>An object of type <typeparamref name="T"/> whose value is represented by <paramref name="value"/>.</returns>
+		public static T Parse<T>(string value) where T : struct, Enum
+		{
+			return (T)Enum.Parse(typeof(T), value);
+		}
+		/// <summary>
+		/// <para>Converts the string representation of the name or numeric value of one or more enumerated constants to an equivalent enumerated object.  A parameter specifies whether the operation is case-insensitive.</para>
+		/// <para>If the given string is invalid for the given enumeration type, an exception is thrown.</para>
+		/// </summary>
+		/// <typeparam name="T">An enumeration type.</typeparam>
+		/// <param name="value">A string containing the name or value to convert.</param>
+		/// <param name="ignoreCase">true to ignore case; false to regard case.</param>
+		/// <returns>An object of type <typeparamref name="T"/> whose value is represented by <paramref name="value"/>.</returns>
+		public static T Parse<T>(string value, bool ignoreCase) where T : struct, Enum
+		{
+			return (T)Enum.Parse(typeof(T), value, ignoreCase);
+		}
+		/// <summary>
+		/// <para>Converts the string representation of the Description attribute, name, or numeric value of one or more enumerated constants to an equivalent enumerated object.  Searches the Description attributes first, then falls back to using standard <c>Enum.Parse()</c>.</para>
+		/// <para>This operation is case-sensitive.</para>
+		/// <para>If the given string is invalid for the given enumeration type, an exception is thrown.</para>
+		/// </summary>
+		/// <typeparam name="T">An enumeration type.</typeparam>
+		/// <param name="value">A string containing the name or value to convert.</param>
+		/// <returns>An object of type <typeparamref name="T"/> whose value is represented by <paramref name="value"/>.</returns>
+		public static T ParseWithDescription<T>(string value) where T : struct, Enum
+		{
+			return ParseWithDescription<T>(value, false);
+		}
+		/// <summary>
+		/// <para>Converts the string representation of the Description attribute, name, or numeric value of one or more enumerated constants to an equivalent enumerated object.  A parameter specifies whether the operation is case-insensitive.  Searches the Description attributes first, then falls back to using standard <c>Enum.Parse()</c>.</para>
+		/// <para>The given <paramref name="value"/> is tested against all enum descriptions first.</para>
+		/// <para>If the given string is invalid for the given enumeration type, an exception is thrown.</para>
+		/// </summary>
+		/// <typeparam name="T">An enumeration type.</typeparam>
+		/// <param name="value">A string containing the name or value to convert.</param>
+		/// <param name="ignoreCase">true to ignore case; false to regard case.</param>
+		/// <returns>An object of type <typeparamref name="T"/> whose value is represented by <paramref name="value"/>.</returns>
+		public static T ParseWithDescription<T>(string value, bool ignoreCase) where T : struct, Enum
+		{
+			if (value == null)
+				throw new ArgumentNullException(nameof(value));
+			if (ignoreCase)
+			{
+				foreach (T val in Enum.GetValues(typeof(T)))
+				{
+					if (value.IEquals(val.GetDescriptionString()))
+					{
+						return val;
+					}
+				}
+			}
+			else
+			{
+				foreach (T val in Enum.GetValues(typeof(T)))
+				{
+					if (value.Equals(val.GetDescriptionString()))
+					{
+						return val;
+					}
+				}
+			}
+			return (T)Enum.Parse(typeof(T), value, ignoreCase);
+		}
+	}
+}
---
+++ UnitTests/TestEnumExtensions.cs
@@ -0,0 +1,140 @@
+﻿using BPUtil;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class TestEnumExtensions
+	{
+		enum TestEnumOne
+		{
+			[Description("A")]
+			A,
+			[Description("B")]
+			B,
+			C
+		}
+		enum TestEnumTwo
+		{
+			[Description("B")]
+			A,
+			[Description("A")]
+			B,
+			C
+		}
+		[TestMethod]
+		public void TestToDescriptionString()
+		{
+			Assert.AreEqual("A", TestEnumOne.A.ToDescriptionString());
+			Assert.AreEqual("B", TestEnumOne.B.ToDescriptionString());
+			Assert.AreEqual("C", TestEnumOne.C.ToDescriptionString());
+			Assert.AreEqual("B", TestEnumTwo.A.ToDescriptionString());
+			Assert.AreEqual("A", TestEnumTwo.B.ToDescriptionString());
+			Assert.AreEqual("C", TestEnumTwo.C.ToDescriptionString());
+		}
+		[TestMethod]
+		public void TestGetDescriptionString()
+		{
+			Assert.AreEqual("A", TestEnumOne.A.GetDescriptionString());
+			Assert.AreEqual("B", TestEnumOne.B.GetDescriptionString());
+			Assert.IsNull(TestEnumOne.C.GetDescriptionString());
+			Assert.AreEqual("B", TestEnumTwo.A.GetDescriptionString());
+			Assert.AreEqual("A", TestEnumTwo.B.GetDescriptionString());
+			Assert.IsNull(TestEnumTwo.C.GetDescriptionString());
+		}
+		[TestMethod]
+		public void TestGetCustomAttributes()
+		{
+			DescriptionAttribute[] descriptionAttributes = TestEnumOne.A.GetCustomAttributes<DescriptionAttribute>().ToArray();
+			Assert.AreEqual(1, descriptionAttributes.Length);
+			Assert.AreEqual("A", descriptionAttributes[0].Description);
+
+			descriptionAttributes = TestEnumOne.B.GetCustomAttributes<DescriptionAttribute>().ToArray();
+			Assert.AreEqual(1, descriptionAttributes.Length);
+			Assert.AreEqual("B", descriptionAttributes[0].Description);
+
+			descriptionAttributes = TestEnumOne.C.GetCustomAttributes<DescriptionAttribute>().ToArray();
+			Assert.AreEqual(0, descriptionAttributes.Length);
+		}
+		[TestMethod]
+		public void TestTryParseWithDescription()
+		{
+			TestEnumOne result1;
+			Assert.IsTrue(EnumExtensions.TryParseWithDescription("A", out result1) && result1 == TestEnumOne.A);
+			Assert.IsTrue(EnumExtensions.TryParseWithDescription("B", out result1) && result1 == TestEnumOne.B);
+			Assert.IsTrue(EnumExtensions.TryParseWithDescription("C", out result1) && result1 == TestEnumOne.C);
+			TestEnumTwo result2;
+			Assert.IsTrue(EnumExtensions.TryParseWithDescription("A", out result2) && result2 == TestEnumTwo.B);
+			Assert.IsTrue(EnumExtensions.TryParseWithDescription("B", out result2) && result2 == TestEnumTwo.A);
+			Assert.IsTrue(EnumExtensions.TryParseWithDescription("C", out result2) && result2 == TestEnumTwo.C);
+		}
+		[TestMethod]
+		public void TestParseWithDescription()
+		{
+			Assert.AreEqual(TestEnumOne.A, EnumExtensions.ParseWithDescription<TestEnumOne>("A"));
+			Assert.AreEqual(TestEnumOne.B, EnumExtensions.ParseWithDescription<TestEnumOne>("B"));
+			Assert.AreEqual(TestEnumOne.C, EnumExtensions.ParseWithDescription<TestEnumOne>("C"));
+			Expect.Exception(() => { EnumExtensions.ParseWithDescription<TestEnumOne>("D"); });
+
+			Assert.AreEqual(TestEnumTwo.B, EnumExtensions.ParseWithDescription<TestEnumTwo>("A"));
+			Assert.AreEqual(TestEnumTwo.A, EnumExtensions.ParseWithDescription<TestEnumTwo>("B"));
+			Assert.AreEqual(TestEnumTwo.C, EnumExtensions.ParseWithDescription<TestEnumTwo>("C"));
+			Expect.Exception(() => { EnumExtensions.ParseWithDescription<TestEnumTwo>("D"); });
+		}
+		[TestMethod]
+		public void TestParse()
+		{
+			Assert.AreEqual(TestEnumOne.A, EnumExtensions.Parse<TestEnumOne>("A"));
+			Assert.AreEqual(TestEnumOne.B, EnumExtensions.Parse<TestEnumOne>("B"));
+			Assert.AreEqual(TestEnumOne.C, EnumExtensions.Parse<TestEnumOne>("C"));
+			Expect.Exception(() => { EnumExtensions.Parse<TestEnumOne>("D"); });
+
+			Assert.AreEqual(TestEnumTwo.A, EnumExtensions.Parse<TestEnumTwo>("A"));
+			Assert.AreEqual(TestEnumTwo.B, EnumExtensions.Parse<TestEnumTwo>("B"));
+			Assert.AreEqual(TestEnumTwo.C, EnumExtensions.Parse<TestEnumTwo>("C"));
+			Expect.Exception(() => { EnumExtensions.Parse<TestEnumTwo>("D"); });
+		}
+		[TestMethod]
+		public void TestParseCaseSensitivity()
+		{
+			// Test functionality of case sensitivity.
+			Expect.Exception(() => { EnumExtensions.Parse<TestEnumTwo>("a", false); });
+			Expect.Exception(() => { EnumExtensions.Parse<TestEnumTwo>("c", false); });
+			Assert.AreEqual(TestEnumTwo.A, EnumExtensions.Parse<TestEnumTwo>("a", true));
+			Assert.AreEqual(TestEnumTwo.C, EnumExtensions.Parse<TestEnumTwo>("c", true));
+
+			// Case sensitivity should be enabled by default.
+			Expect.Exception(() => { EnumExtensions.Parse<TestEnumTwo>("a"); });
+		}
+		[TestMethod]
+		public void TestParseWithDescriptionCaseSensitivity()
+		{
+			// Test functionality of case sensitivity.
+			Expect.Exception(() => { EnumExtensions.ParseWithDescription<TestEnumTwo>("a", false); });
+			Expect.Exception(() => { EnumExtensions.ParseWithDescription<TestEnumTwo>("c", false); });
+			Assert.AreEqual(TestEnumTwo.B, EnumExtensions.ParseWithDescription<TestEnumTwo>("a", true));
+			Assert.AreEqual(TestEnumTwo.C, EnumExtensions.ParseWithDescription<TestEnumTwo>("c", true));
+
+			// Case sensitivity should be enabled by default.
+			Expect.Exception(() => { EnumExtensions.ParseWithDescription<TestEnumTwo>("a"); });
+		}
+		[TestMethod]
+		public void TestTryParseWithDescriptionCaseSensitivity()
+		{
+			// Test functionality of case sensitivity.
+			Assert.IsFalse(EnumExtensions.TryParseWithDescription<TestEnumTwo>("a", false, out TestEnumTwo _));
+			Assert.IsFalse(EnumExtensions.TryParseWithDescription<TestEnumTwo>("c", false, out TestEnumTwo _));
+			Assert.IsTrue(EnumExtensions.TryParseWithDescription<TestEnumTwo>("A", true, out TestEnumTwo _1) && _1 == TestEnumTwo.B);
+			Assert.IsTrue(EnumExtensions.TryParseWithDescription<TestEnumTwo>("C", true, out TestEnumTwo _2) && _2 == TestEnumTwo.C);
+
+			// Case sensitivity should be enabled by default.
+			Assert.IsFalse(EnumExtensions.TryParseWithDescription<TestEnumTwo>("a", out TestEnumTwo _));
+		}
+	}
+}
