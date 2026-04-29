77d6435c84598976218e5f7c5a1a38a1b405d3a6
bp2008 <bp2008@users.noreply.github.com>
2026-04-29 10:05:37 -0600

    Added EventExtensions class with SafeInvoke method. Added unit tests for SafeInvoke.

---
+++ BPUtil/EventExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace BPUtil
+{
+	/// <summary>
+	/// Extension methods for events.
+	/// </summary>
+	public static class EventExtensions
+	{
+		/// <summary>
+		/// Invokes each subscriber and catches exceptions thrown by handlers so one failure doesn't stop the chain.
+		/// </summary>
+		/// <param name="handler">The event handler to invoke.</param>
+		/// <param name="sender">The source of the event (usually <c>this</c> in the caller's context)</param>
+		/// <param name="args">The event arguments to pass to the subscribers.</param>
+		/// <param name="onError">(Optional) exception handling method.  If null, exceptions thrown by event handlers will be silently ignored.</param>
+		public static void SafeInvoke<T>(this EventHandler<T> handler, object sender, T args, Action<Exception> onError = null)
+		{
+			if (handler == null)
+				return;
+
+			foreach (Delegate subscriber in handler.GetInvocationList())
+			{
+				try
+				{
+					// DynamicInvoke is used to work with the Delegate type returned by GetInvocationList
+					subscriber.DynamicInvoke(sender, args);
+				}
+				catch (System.Reflection.TargetInvocationException ex)
+				{
+					if (onError != null)
+					{
+						// Exceptions thrown by DynamicInvoke are wrapped; we want the actual error
+						Exception actualException = ex.InnerException ?? ex;
+						onError(actualException);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ UnitTests/TestEventExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BPUtil;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class TestEventExtensions
+	{
+		[TestMethod]
+		public void SafeInvoke_NullHandler_DoesNotThrow()
+		{
+			EventHandler<EventArgs> handler = null;
+			handler.SafeInvoke(this, EventArgs.Empty);
+		}
+
+		[TestMethod]
+		public void SafeInvoke_SingleSubscriber_IsInvoked()
+		{
+			bool invoked = false;
+			EventHandler<EventArgs> handler = (s, e) => { invoked = true; };
+			handler.SafeInvoke(this, EventArgs.Empty);
+			Assert.IsTrue(invoked);
+		}
+
+		[TestMethod]
+		public void SafeInvoke_MultipleSubscribers_AllInvoked()
+		{
+			int count = 0;
+			EventHandler<EventArgs> handler = null;
+			handler += (s, e) => { count++; };
+			handler += (s, e) => { count++; };
+			handler += (s, e) => { count++; };
+			handler.SafeInvoke(this, EventArgs.Empty);
+			Assert.AreEqual(3, count);
+		}
+
+		[TestMethod]
+		public void SafeInvoke_SubscriberThrows_OtherSubscribersStillInvoked()
+		{
+			List<int> results = new List<int>();
+			EventHandler<EventArgs> handler = null;
+			handler += (s, e) => { results.Add(1); };
+			handler += (s, e) => { throw new InvalidOperationException("fail"); };
+			handler += (s, e) => { results.Add(3); };
+			handler.SafeInvoke(this, EventArgs.Empty);
+			CollectionAssert.AreEqual(new[] { 1, 3 }, results);
+		}
+
+		[TestMethod]
+		public void SafeInvoke_SubscriberThrows_OnErrorReceivesInnerException()
+		{
+			List<Exception> errors = new List<Exception>();
+			EventHandler<EventArgs> handler = (s, e) => { throw new ArgumentException("test error"); };
+			handler.SafeInvoke(this, EventArgs.Empty, ex => errors.Add(ex));
+			Assert.AreEqual(1, errors.Count);
+			Assert.IsInstanceOfType(errors[0], typeof(ArgumentException));
+			Assert.AreEqual("test error", errors[0].Message);
+		}
+
+		[TestMethod]
+		public void SafeInvoke_SubscriberThrows_NullOnError_ExceptionSilentlyIgnored()
+		{
+			EventHandler<EventArgs> handler = (s, e) => { throw new Exception("ignored"); };
+			handler.SafeInvoke(this, EventArgs.Empty, null);
+		}
+
+		[TestMethod]
+		public void SafeInvoke_PassesSenderAndArgsCorrectly()
+		{
+			object receivedSender = null;
+			EventArgs receivedArgs = null;
+			EventHandler<EventArgs> handler = (s, e) => { receivedSender = s; receivedArgs = e; };
+			var args = new EventArgs();
+			handler.SafeInvoke("mySender", args);
+			Assert.AreEqual("mySender", receivedSender);
+			Assert.AreSame(args, receivedArgs);
+		}
+	}
+}
+
+namespace UnitTests.AppUtilitiesTests
+{
+}
